62d2a23456aea9eccad2c2a7fc094dc28806498b
akhmadjonjurayev <formyproject1999@gmail.com>
2021-03-24 15:44:52 +0500

    something

---
+++ Controllers/BotController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram_Bot_1.Service;
+
+namespace Telegram_Bot_1.Controllers
+{
+    [Route("api/bot")]
+    [ApiController]
+    public class BotController : ControllerBase
+    {
+        private readonly ITelegramBotClient _client;
+        private readonly ICommandService _command;
+        public BotController(ITelegramBotClient client,ICommandService command)
+        {
+            _command = command;
+            _client = client;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Started");
+        }
+        [HttpPost("post")]
+        public async Task<IActionResult> Post([FromBody] Update update)
+        {
+            if (update == null) return Ok();
+            var message = update.Message;
+            foreach(var command in _command.Get())
+            {
+                if(command.Contains(message))
+                {
+                    await command.Execute(message, _client);
+                    break;
+                }
+            }
+            return Ok();
+        }
+    }
+}
---
+++ ServiceCollectionExtention.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace Telegram_Bot_1
+{
+    public static class ServiceCollectionExtention
+    {
+        public static IServiceCollection AddTelegramBot(this IServiceCollection services,IConfiguration configuration)
+        {
+            var client = new TelegramBotClient(configuration["Token"]);
+            var webHook = $"{configuration["Url"]}/api/bot/post";
+            client.SetWebhookAsync(webHook).Wait();
+           
+            return services.AddSingleton<ITelegramBotClient>(client);
+        }
+    }
+}
